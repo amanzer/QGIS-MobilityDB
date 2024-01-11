fce2cd1735517021a96b2c6b1ce586055f2a8e41
amanzer <ali.iman.manzer@ulb.Be>
2024-01-11 14:38:56 +0100

    deleting unused code

--- luderic/withoutconnection.py
+++ luderic/withoutconnection.py
@@ -46,28 +46,3 @@ trajectories = (
 )
 trajectories["distance"] = trajectories["trajectory"].apply(lambda t: t.length())
 rows = [ais.iloc[0]]
-
-
-
-canvas = iface.mapCanvas()
-temporalController = canvas.temporalController()
-currentFrameNumber = temporalController.currentFrameNumber()
-
-
-
-## Create a temporal layer (variable 'vlayer') with single field for datetime
-vlayer = QgsVectorLayer("Point", "points_3", "memory")
-pr = vlayer.dataProvider()
-pr.addAttributes([QgsField("time", QVariant.DateTime)])
-vlayer.updateFields()
-tp = vlayer.temporalProperties()
-tp.setIsActive(True)
-tp.setMode(Qgis.VectorTemporalMode.FixedTemporalRange)
-#tp.setMode(1) #single field with datetime
-tp.setStartField("time")
-crs = vlayer.crs()
-crs.createFromId(22992)
-vlayer.setCrs(crs)
-vlayer.updateFields()
-QgsProject.instance().addMapLayer(vlayer)
-
