3cdcbb928e2dfe3fa72a1f7d53ed8c0d2d4a6380
amanzer <ali.iman.manzer@ulb.be>
2024-05-07 12:33:26 +0200

    Updated pg_tile query

--- qViz/pg_tile.sql
+++ qViz/pg_tile.sql
@@ -1,21 +1,27 @@
+
+
 CREATE OR REPLACE
-FUNCTION public.simplAnim(
+FUNCTION public.VECTOR_TILE(
             z integer, x integer, y  integer,mmsi_ integer,p_start text, p_end text)
 RETURNS bytea
 AS $$
     WITH bounds AS (
         SELECT ST_TileEnvelope(z,x,y) as geom
     ),
+    trips_ AS (
+        SELECT * from pymeos_demo as a where a.mmsi = mmsi_ or mmsi_ = 2190045
+    )
+    ,
     vals AS (
         SELECT mmsi,numInstants(trip) as size,asMVTGeom(transform(attime(trip,span(p_start::timestamptz, p_end::timestamptz, true, true)),3857), transform((bounds.geom)::stbox,3857))
             as geom_times
         FROM (
             SELECT
             mmsi,
-            trajectory AS trip
+            trajectory::tgeompoint AS trip
             FROM
+            trips_
 
-            pymeos_demo
         ) as ego, bounds
     ),
     mvtgeom AS (
@@ -28,38 +34,7 @@ SELECT ST_AsMVT(mvtgeom) FROM mvtgeom
 STABLE
 PARALLEL SAFE;
 
-%
 
-CREATE OR REPLACE
-FUNCTION public.simplAnim2(
-            z integer, x integer, y  integer,mmsi_ integer)
-RETURNS bytea
-AS $$
-    WITH bounds AS (
-        SELECT ST_TileEnvelope(z,x,y) as geom
-    ),
-    trips_ AS (
-        SELECT *,1 AS index from pymeos_demo as a where a.mmsi = mmsi_ or mmsi_ = -1
-    )
-    ,
-    vals AS (
-        SELECT mmsi,index,numInstants(trip) as size,asMVTGeom(transform(trip::tgeompoint), transform((bounds.geom)::stbox,3857))
-            as geom_times
-        FROM (
-            SELECT
-            mmsi,index,
-            trajectory::tgeompoint AS trip
-            FROM
-            trips_
+---
 
-        ) as ego, bounds
-    ),
-    mvtgeom AS (
-        SELECT (geom_times).geom, (geom_times).times,index,size,mmsi
-        FROM vals
-    )
-SELECT ST_AsMVT(mvtgeom) FROM mvtgeom
-                                  $$
-    LANGUAGE 'sql'
-STABLE
-PARALLEL SAFE;
+SELECT public.VECTOR_TILE(0, 0, 0, 2190045, '2023-06-01 00:00:00', '2023-06-02 00:00:00');
